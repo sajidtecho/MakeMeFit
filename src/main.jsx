ce945d70cc85966ebb92e32ce699ecd550ef54e8
Sajid <sajidahmadgpt@gmail.com>
2026-04-10 09:42:54 +0530

    feat: initialize React root with BrowserRouter and Vercel SpeedInsights

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
+      <SpeedInsights />
     </BrowserRouter>
   </React.StrictMode>,
 )
